80cff0baf4a5f5197f6cdbae90e96313dd632cb4
JinseoPark <rarebook92@naver.com>
2020-12-07 18:41:56 +0900

    Initial Commit

---
+++ CodeSquad/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CodeSquad
+//
+//  Created by jinseo park on 12/7/20.
+//  Copyright © 2020 JacksonKR. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
