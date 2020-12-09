650f0f8f8db6952c2ca38468a63ea9770ad49f3b
JinseoPark <rarebook92@naver.com>
2020-12-09 18:04:27 +0900

    Created cube class and print func
    큐브 클래스를 생성하고 초기화 및 현재 큐브클래스의 값을 확인 할 수 있는 print문을 제작함

--- CodeSquad/main.swift
+++ CodeSquad/main.swift
@@ -5,8 +5,39 @@
 //  Created by jinseo park on 12/7/20.
 //  Copyright © 2020 JacksonKR. All rights reserved.
 //
+//2단계: 평면 큐브 구현하기
+
+//Cube의 모양
+//R R W
+//G C W
+//G B B
 
 import Foundation
 
-print("Hello, World!")
+
+class Cube{
+    var myCube = [[String]]() //2차원 문자열 배열 생성
+    init(){
+        self.myCube = [["R","R","W"],["G","C","W"],["G","B","B"]]
+    }
+    
+    func printMyCube(){ //2차원 배열을 가시적으로 확인 하도록 프린트할 수 있는 함수.
+        
+        for i in 0..<myCube.count{
+            var line = ""
+            for j in 0..<myCube[i].count{
+                line += myCube[i][j]
+                line += " "
+            }
+            print(line)
+        }
+    }
+}
+
+
+let mycube = Cube()
+mycube.printMyCube()
+
+//var a = "A"
+//print(a)
 
