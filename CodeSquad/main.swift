c43ba8afc7f600783648c227b470d96186cb6aea
JinseoPark <rarebook92@naver.com>
2020-12-13 17:24:17 +0900

    Created switchCube and switchCenterCube
    큐브의 회전을 담당하는 함수 두개를 생성함. switchCube에서 큐브 기호에 따라 회전하며, switchCenterCube함수에서 중심이 되는 면의 값을 변경한다. 현재 U / U'의 값을 생성함

--- CodeSquad/main.swift
+++ CodeSquad/main.swift
@@ -5,12 +5,10 @@
 //  Created by jinseo park on 12/7/20.
 //  Copyright © 2020 JacksonKR. All rights reserved.
 //
-//2단계: 평면 큐브 구현하기
 
-//Cube의 모양
-//R R W
-//G C W
-//G B B
+////                (self.myCube[0][0][0], self.myCube[0][0][1], self.myCube[0][0][2], self.myCube[0][1][0], self.myCube[0][1][2], self.myCube[0][2][0], self.myCube[0][2][1], self.myCube[0][2][2]) =
+//                    (self.myCube[0][2][0],self.myCube[0][1][0],self.myCube[0][0][0], self.myCube[0][2][1], self.myCube[0][0][1], self.myCube[0][2][2], self.myCube[0][1][2], self.myCube[0][0][2])
+//
 
 import Foundation
 
@@ -34,11 +32,79 @@ extension String{ //regix 정규식 문법 관련 함수
 class Cube{
     
     var myCube = [[[String]]]() //3x3 큐브
+    
+    var countRoatation : Int = 0
     //var inputChar : String = ""
     init(){
-        self.myCube = [[["B","B","B"],["B","B","B"],["B","B","B"]],[["W","W","W"],["W","W","W"],["W","W","W"]],[["O","O","O"],["O","O","O"],["O","O","O"]],[["G","G","G"],["G","G","G"],["G","G","G"]],[["Y","Y","Y"],["Y","Y","Y"],["Y","Y","Y"]],[["R","R","R"],["R","R","R"],["R","R","R"]]]
+        //        self.myCube = [[["B","B","B"],["B","B","B"],["B","B","B"]],[["W","W","W"],["W","W","W"],["W","W","W"]],[["O","O","O"],["O","O","O"],["O","O","O"]],[["G","G","G"],["G","G","G"],["G","G","G"]],[["Y","Y","Y"],["Y","Y","Y"],["Y","Y","Y"]],[["R","R","R"],["R","R","R"],["R","R","R"]]]
+        self.myCube = [[["1","2","3"],["4","C","6"],["7","8","9"]],[["W","W","W"],["W","W","W"],["W","W","W"]],[["O","O","O"],["O","O","O"],["O","O","O"]],[["G","G","G"],["G","G","G"],["G","G","G"]],[["Y","Y","Y"],["Y","Y","Y"],["Y","Y","Y"]],[["R","R","R"],["R","R","R"],["R","R","R"]]]
+        
     }
-    
+    func switchCubeCenter(CIndex : Int ,direction : String){
+        
+        if(direction == "L"){ //clock
+            (self.myCube[CIndex][0][0], self.myCube[CIndex][0][1], self.myCube[CIndex][0][2], self.myCube[CIndex][1][0], self.myCube[CIndex][1][2], self.myCube[CIndex][2][0], self.myCube[CIndex][2][1], self.myCube[CIndex][2][2]) =
+                (self.myCube[CIndex][2][0],self.myCube[CIndex][1][0],self.myCube[CIndex][0][0], self.myCube[CIndex][2][1], self.myCube[CIndex][0][1], self.myCube[CIndex][2][2], self.myCube[CIndex][1][2], self.myCube[CIndex][0][2])
+            
+        }else if(direction == "R"){//counter-clock
+            (self.myCube[CIndex][0][0], self.myCube[CIndex][0][1], self.myCube[CIndex][0][2], self.myCube[CIndex][1][0], self.myCube[CIndex][1][2], self.myCube[CIndex][2][0], self.myCube[CIndex][2][1], self.myCube[CIndex][2][2]) =
+            (self.myCube[CIndex][0][2],self.myCube[CIndex][1][2],self.myCube[CIndex][2][2], self.myCube[CIndex][0][1], self.myCube[CIndex][2][1], self.myCube[CIndex][0][0], self.myCube[CIndex][1][0], self.myCube[CIndex][2][0])
+        }
+    }
+    func switchCube(inputWords : String){
+        
+        var disassembleStrs : [String] = []
+        
+        disassembleStrs = inputWords.getArrayAfterRegex(regex: "[FRUBLD][1-9]?[/']?")
+        
+        countRoatation += disassembleStrs.count
+        
+        print(disassembleStrs)
+        
+        for item in disassembleStrs { //disassembleStrs 순서대로 큐브를 바꾼다.
+            
+            switch item {
+            case "U":
+                //1. 가장 윗면 바꾸기.
+                //2. 윗면에 포함된 중앙4면을 바꾸기
+                print("U : 가장 위 큐브 시계방향 회전" )
+                
+                switchCubeCenter(CIndex: 0 , direction: "L")
+                (self.myCube[1][0],self.myCube[2][0],self.myCube[3][0],self.myCube[4][0])=(self.myCube[2][0],self.myCube[3][0],self.myCube[4][0],self.myCube[1][0])
+                                
+            case "U'":
+                print("U : 가장 위 큐브 시계반대방향 회전" )
+                switchCubeCenter(CIndex: 0, direction: "R")
+                (self.myCube[1][0],self.myCube[2][0],self.myCube[3][0],self.myCube[4][0])=(self.myCube[4][0],self.myCube[1][0],self.myCube[2][0],self.myCube[3][0])
+//            case "R":
+//                print("R : 가장 오른쪽 줄을 위로 한 칸 밀기" )
+//                (self.myCube[0][2], self.myCube[1][2], self.myCube[2][2]) = (self.myCube[1][2], self.myCube[2][2], self.myCube[0][2])
+//            case "R'":
+//                print("R' : 가장 오른쪽 줄을 아래로 한 칸 밀기" )
+//                (self.myCube[0][2], self.myCube[1][2], self.myCube[2][2]) = (self.myCube[2][2], self.myCube[0][2], self.myCube[1][2])
+//            case "L":
+//                print("L : 가장 왼쪽 줄을 아래로 한 칸 밀기" )
+//                (self.myCube[0][0], self.myCube[1][0], self.myCube[2][0]) = (self.myCube[2][0], self.myCube[0][0], self.myCube[1][0])
+//            case "L'":
+//                print("L' : 가장 왼쪽 줄을 위로 한 칸 밀기" )
+//                (self.myCube[0][0], self.myCube[1][0], self.myCube[2][0]) = (self.myCube[1][0], self.myCube[2][0], self.myCube[0][0])
+//            case "B":
+//                print("B : 가장 아랫줄을 오른쪽으로 한 칸 밀기 " )
+//                (self.myCube[2][0], self.myCube[2][1], self.myCube[2][2]) = (self.myCube[2][2], self.myCube[2][0], self.myCube[2][1])
+//            case "B'":
+//                print("B' : 가장 아랫줄을 왼쪽으로 한 칸 밀기" )
+//                (self.myCube[2][0], self.myCube[2][1], self.myCube[2][2]) = (self.myCube[2][1], self.myCube[2][2], self.myCube[2][0])
+//
+            default:
+                print("default")
+            }
+            
+            
+        }
+        //print(disassembleStrs)
+        //정규식을 사용해서 문자를 자른다. ' 가 옵션이다.
+        printMyCube()
+    }
     
     func checkReadLine(){ //사용자로부터 값을 받아서 규약된 값인지 체크하는 함수, 최소한 하나의 명령어가 입력이 되면 switch 함수로 넘긴다.
         while(true){
@@ -48,6 +114,7 @@ class Cube{
             if let inputWords = inputWords{
                 //Q , q 를 입력하면 프로그램 종료
                 if(inputWords == "Q" || inputWords == "q"){
+                    print("조작갯수 : \(countRoatation)")
                     print("이용해주셔서 감사합니다. 뚜뚜뚜.")
                     exit(0)
                 }else{
@@ -56,16 +123,13 @@ class Cube{
                     let charset = CharacterSet(charactersIn: "FRUBLD'") //받아야 하는 input이 다음 캐릭터셋 안에 있어야만 한다.
                     
                     if((inputWordsUpperCase.rangeOfCharacter(from: charset)) != nil){
-                        print(inputWordsUpperCase)
+                        switchCube(inputWords: inputWordsUpperCase)
                         
                     }else{
                         print("None Chars supposed to be in inputwords")
                         continue
                     }
-                    
-                    
                 }
-                
             }
         }
     }
@@ -91,6 +155,7 @@ class Cube{
 
 let mycube = Cube()
 
+mycube.printMyCube()
 mycube.checkReadLine()
 
 
